fa0346a8bcfc34d90a4afb4388deb9e049fca327
Farhan Mansuri <mansurifarhanfm@gmail.com>
2022-09-23 15:05:42 +0530

    hi

--- pages/player.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactPlayer from 'react-player'
-
-// Only loads the YouTube player
-<ReactPlayer url='https://vidstreamingcdn.com/cdn34/a96411258da4b8a75319906d0cc507f7/EP.18.v0.1644104042.360p.mp4?mac=7GmeilE5nn5L7xGZqxt4YNTnzQ53eEazGha0ZBD15WU%3D&vip=&expiry=1644122389382' />
