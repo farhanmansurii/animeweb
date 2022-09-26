2f921a279f764d1deeca00bd88de80c40ab07a39
Farhan Mansuri <mansurifarhanfm@gmail.com>
2022-09-26 20:43:19 +0530

    hi

--- pages/components/AnimeCard.js
+++ pages/components/AnimeCard.js
@@ -1,30 +1,19 @@
-import { Button, Flex, Text, Image } from "@chakra-ui/react";
+import { Box, Flex, Text, Image } from "@chakra-ui/react";
 import Link from "next/link";
 import React from "react";
 function AnimeCard({ animeImg, title, id }) {
   return (
     <>
       <Link href={`/details?id=${id}`}>
-        <Flex
-          flexDir="column"
-          p="3"
-          _hover={{ borderRadius: "10px" }}
-          width='100%'
-          height='100%'
-          align="center"
-        >
-          <Image
-            src={animeImg}
-            borderRadius="xl"
-            _hover={{ height: "110%", width: "110%", transition: "0.2s" }}
-            height= "100%" width= "100%"
-            alt=""
-          />
-          <Text noOfLines={1} >
+      <Box display='flex' height='300px'  width='200px'  alignSelf='center' bgPos='center' bgGradient='linear(to-t, black, rgba(255, 255, 255, .05))' m='2rem' >
+      <Box bgSize='cover' bgRepeat="no-repeat"  height='300px'  width='200px' zIndex='-1'   backgroundImage={animeImg}  >
+      <Flex justify='space-between'alignSelf='self-end'>
+        <Text noOfLines={1} >
             {title}
-          </Text>
-        </Flex>
-      </Link>
+          </Text><Text  variant='unstyled' alignSelf='flex-end'>+</Text>
+          </Flex>
+      </Box>
+    </Box></Link>
     </>
   );
 }
