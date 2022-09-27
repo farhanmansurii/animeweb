74a292ca93f7620ea54f75028ef891092983f6db
Farhan Mansuri <mansurifarhanfm@gmail.com>
2022-09-27 15:12:42 +0530

    hi

--- pages/components/AnimeCard.js
+++ pages/components/AnimeCard.js
@@ -5,7 +5,7 @@ function AnimeCard({ animeImg, title, id }) {
   return (
     <>
       <Link href={`/details?id=${id}`}>
-      <Box display='flex' height='300px' textAlign='center' borderRadius='xl' flexDir='column-reverse' bgSize='cover' bgRepeat="no-repeat" mt='4rem'  width={['180px', null, '200px']} alignSelf='center' backgroundImage={animeImg} bgPos='center' >
+      <Box display='flex' height='300px' textAlign='center' borderRadius='xl' flexDir='column-reverse' bgSize='cover' bgRepeat="no-repeat" mt='4rem'  width={['170px', null, '200px']} alignSelf='center' backgroundImage={animeImg} bgPos='center' >
 
             
       <Text position='absolute' color='white' pt='6' pb='3' px='1' width='inherit' borderBottomRadius='inherit' bgGradient='linear(to-t, black, rgba(255, 255, 255, .05))'>
