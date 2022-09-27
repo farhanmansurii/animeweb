63bf492e046ec5dc5112319e7fb576f2daa34870
Farhan Mansuri <mansurifarhanfm@gmail.com>
2022-09-27 15:09:47 +0530

    hi

--- pages/components/AnimeCard1.js
+++ pages/components/AnimeCard1.js
@@ -1,16 +1,13 @@
-import { Box, Button, Container, Flex ,Text} from '@chakra-ui/react'
+import { Box, Button, Container, Flex ,Spacer,Text} from '@chakra-ui/react'
 import React from 'react'
 
 const AnimeCard1 = () => {
   return (<>
-    <Box display='flex' height='300px'  width='200px'  alignSelf='center' bgPos='center' bgGradient='linear(to-t, black, rgba(255, 255, 255, .05))' >
-     <Box bgSize='cover' bgRepeat="no-repeat"  height='300px'  width='200px' zIndex='-1'   backgroundImage='https://gogocdn.net/cover/made-in-abyss-retsujitsu-no-ougonkyou.png'  >
-      <Flex justify='space-between' alignSelf='flex-end'>
-
-      <Text>Hi Episode Name</Text> 
-      <Text  variant='unstyled' alignSelf='flex-end'>+</Text>
-      </Flex>
-      </Box>
+    <Box display='flex' height='300px' border='100px' flexDir='column-reverse' bgSize='cover'   bgRepeat="no-repeat"  mt='4rem' width='200px'  alignSelf='center'    backgroundImage='https://gogocdn.net/cover/made-in-abyss-retsujitsu-no-ougonkyou.png'  bgPos='center' >
+     
+      <Text position='absolute' color='white' width='fit-content' bgGradient='linear(to-t, black, rgba(255, 255, 255, .05))'>
+        <Spacer height='100px'/>
+        Hi Episode Name</Text> 
     </Box>
   </>
   )
