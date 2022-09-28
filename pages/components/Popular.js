00961968f6680f750ffd659483e9320a6909e040
farhan <mansurifarhanfm@gmail.com>
2022-09-28 23:31:32 +0530

    hi

--- pages/components/Popular.js
+++ pages/components/Popular.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Flex, VStack, Grid, SimpleGrid } from "@chakra-ui/react";
 import AnimeCard from "./AnimeCard";
+import { useState } from "react";
 const Popular = ({ baseURL }) => {
   const [pageApi, setPageApi] = useState(1);
   const [list, setList] = React.useState([]);
   React.useEffect(() => {
     fetch(baseURL)
       .then((response) => response.json())
-      .then((animelist) =>setList(animelist));
+      .then((animelist) => setList(animelist));
     return () => {};
   }, [baseURL]);
   return (
-    <SimpleGrid  minChildWidth="200px" >
-      {list.map((ele) => 
+    <SimpleGrid minChildWidth="200px">
+      {list.map((ele) => (
         <VStack key={ele.animeId}>
           <AnimeCard
             animeImg={ele.animeImg}
@@ -20,7 +21,7 @@ const Popular = ({ baseURL }) => {
             id={ele.animeId}
           />
         </VStack>
-      )}
+      ))}
     </SimpleGrid>
   );
 };
