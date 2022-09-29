b58835da2b0bb20e66659ee020c62e7464fcc262
farhan <mansurifarhanfm@gmail.com>
2022-09-30 02:10:33 +0530

    hi

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,133 @@ import { SunIcon } from "@chakra-ui/icons";
 import React from "react";
 import { Swiper, SwiperSlide, FreeMode } from "swiper/react";
 import "swiper/css";
-import Episode from "./components/Episode";
-import Row from "./components/Row";
 export default function Home({ popular, action, adventure }) {
   return (
     <>
       <div className="mt-4">
-        <Row popular={popular} text="Popular Anime" />
-        <Row popular={action} text="Action Anime" />
-        <Row popular={adventure} text="Adventure Anime" />
+        <div className="mt-4 mb-10 w-10/12 mx-auto">
+          <div className="text-3xl font-semibold my-3">Popular Anime</div>
+          <Swiper
+            slidesPerView={7}
+            spaceBetween={35}
+            breakpoints={{
+              "@0.00": {
+                slidesPerView: 4.4,
+                spaceBetween: 6,
+              },
+              "@0.75": {
+                slidesPerView: 5,
+                spaceBetween: 20,
+              },
+              "@1.00": {
+                slidesPerView: 6,
+                spaceBetween: 35,
+              },
+              "@1.30": {
+                slidesPerView: 6,
+                spaceBetween: 35,
+              },
+              "@1.50": {
+                slidesPerView: 9,
+                spaceBetween: 35,
+              },
+            }}
+            s
+            className="mySwiper"
+          >
+            {popular.map((e) => (
+              <SwiperSlide key={e.malId}>
+                <AnimeCard
+                  id={e.malId}
+                  title={e.title.english}
+                  animeImg={e.image}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className="mt-4 mb-10 w-10/12 mx-auto">
+          <div className="text-3xl font-semibold my-3">Action Anime</div>
+          <Swiper
+            slidesPerView={7}
+            spaceBetween={35}
+            breakpoints={{
+              "@0.00": {
+                slidesPerView: 4.4,
+                spaceBetween: 6,
+              },
+              "@0.75": {
+                slidesPerView: 5,
+                spaceBetween: 20,
+              },
+              "@1.00": {
+                slidesPerView: 6,
+                spaceBetween: 35,
+              },
+              "@1.30": {
+                slidesPerView: 6,
+                spaceBetween: 35,
+              },
+              "@1.50": {
+                slidesPerView: 9,
+                spaceBetween: 35,
+              },
+            }}
+            s
+            className="mySwiper"
+          >
+            {action.map((e) => (
+              <SwiperSlide key={e.malId}>
+                <AnimeCard
+                  id={e.malId}
+                  title={e.title.english}
+                  animeImg={e.image}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className="mt-4 mb-10 w-10/12 mx-auto">
+          <div className="text-3xl font-semibold my-3">Adventure Anime</div>
+          <Swiper
+            slidesPerView={7}
+            spaceBetween={35}
+            breakpoints={{
+              "@0.00": {
+                slidesPerView: 4.4,
+                spaceBetween: 6,
+              },
+              "@0.75": {
+                slidesPerView: 5,
+                spaceBetween: 20,
+              },
+              "@1.00": {
+                slidesPerView: 6,
+                spaceBetween: 35,
+              },
+              "@1.30": {
+                slidesPerView: 6,
+                spaceBetween: 35,
+              },
+              "@1.50": {
+                slidesPerView: 9,
+                spaceBetween: 35,
+              },
+            }}
+            s
+            className="mySwiper"
+          >
+            {adventure.map((e) => (
+              <SwiperSlide key={e.malId}>
+                <AnimeCard
+                  id={e.malId}
+                  title={e.title.english}
+                  animeImg={e.image}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </>
   );
