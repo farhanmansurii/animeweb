d4eb451453a522726366133f98bc4bfc4d98e5f3
farhan <mansurifarhanfm@gmail.com>
2022-09-30 06:10:17 +0530

    hi

--- pages/components/Episode.js
+++ pages/components/Episode.js
@@ -1,14 +1,21 @@
 import { Container, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
-const Episode = ({ image, number, title,description }) => {
-  return (<>
-         
-    <div className=" flex flex-col-reverse h-[150px] min-w-[220px] m-1 p-3 whitespace-nowrap"   style={{ backgroundImage: `url(${image})` }} >
-      <div className="self-bottom text-sm text-white mx-2 hidden  ">{description}</div>
-      <div className="self-bottom  text-white text-md mx-2 text-shadow-xl whitespace-pre-wrap">Ep {number} : {title}</div>
-    </div>
-  </>
+const Episode = ({ image, number, title, description }) => {
+  return (
+    <>
+      <div
+        className=" flex flex-col-reverse h-[150px] min-w-[220px] m-1 p-3 whitespace-nowrap"
+        style={{ backgroundImage: `url(${image})` }}
+      >
+        <div className="self-bottom text-sm text-white mx-2 hidden  ">
+          {description}
+        </div>
+        <div className="self-bottom  text-white text-md mx-2 text-shadow-xl whitespace-pre-wrap">
+          Ep {number} : {title}
+        </div>
+      </div>
+    </>
   );
 };
 
