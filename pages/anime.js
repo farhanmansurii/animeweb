b68a240084fd3e6cbd171e6cf3a0a916db9289fb
farhan <mansurifarhanfm@gmail.com>
2022-10-03 22:28:09 +0530

    hi

--- pages/anime.js
+++ pages/anime.js
@@ -2,10 +2,6 @@ import React, { useEffect } from "react";
 import Link from "next/link";
 import requests from "./api/requests";
 function anime({ results }) {
-  return (
-
-   
-    </>
-  );
+  return <div>anime</div>;
 }
 export default anime;
