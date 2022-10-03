909a71a9e8bb8dc66945b7f611fd750e44ce3832
farhan <mansurifarhanfm@gmail.com>
2022-10-03 22:25:09 +0530

    hi

--- pages/anime.js
+++ pages/anime.js
@@ -1,24 +1,11 @@
 import React, { useEffect } from "react";
-import GenreSelector from "./components/GenreSelector";
-import AnimeCard from "./components/AnimeCard";
 import Link from "next/link";
 import requests from "./api/requests";
 function anime({ results }) {
+  return (
 
-  return <><GenreSelector />
-    <div className="p-5 grid mx-auto my-10 grid-cols-2 gap-4 md:grid-cols-6 w-11/12 lg:w-10/12">
-
-      {results.map((e) =>
-        <AnimeCard animeImg={e.image} title={e.title.english || e.title.native} key={e.malId} rating={e.rating} id={e.malId} />)}
-    </div></>;
-}
-export async function getServerSideProps(context) {
-  const genre = context.query.genre
-  const request = await fetch(requests[genre]?.url || requests.popular.url).then((res) => res.json())
-  return {
-    props: {
-      results: request.results
-    },
-  };
+   
+    </>
+  );
 }
 export default anime;
